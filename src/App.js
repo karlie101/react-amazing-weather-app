2b0fc2caae19a429023b4d955343bb386588fd03
karlie101 <129705716+karlie101@users.noreply.github.com>
2023-08-04 12:42:20 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return ( 
+  <div className="App">
+   <h1> Weather App </h1>
+    <footer>
+      This project was coded by Karlie Howard and is {" "}
+      <a href="https://github.com/karlie101/react-amazing-weather-app"
+         target="_blank" rel="noreferrer"
+         >
+          open-sourced on GitHub
+         </a>
+    </footer>
+    </div>
+  );
 }
 
 
